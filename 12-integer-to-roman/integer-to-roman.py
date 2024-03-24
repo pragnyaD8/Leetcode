9f337e595e27204bd3c36b00127439112b8cf794
pragnya reddy <66670412+pragnyaD8@users.noreply.github.com>
2024-03-24 10:31:58 -0500

    Time: 46 ms (63.86%) | Memory: 16.4 MB (94.63%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,11 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+      d={1000:"M",900:"CM",500:"D",400:"CD",100:"C",90:"XC",50:"L",40:"XL",10:"X",9:"IX",5:"V",4:"IV",1:"I"}
+      p=""
+      for k,v in d.items():
+          while k <=num:
+              p=p+v
+              num=num-k
+      return p
+      
+     
